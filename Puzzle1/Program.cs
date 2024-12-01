a341760dffbb187c9d79f197427dd578466b4863
Benjamin Klein <benjamin.klein@capgemini.com>
2024-12-01 12:40:36 +0100

    added setup & puzzle 1

---
+++ Puzzle1/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string filePath = "input.txt";
+
+List<int> left = new List<int>();;
+List<int> right = new List<int>();
+
+foreach (string line in File.ReadLines(filePath)) {
+    parse(line);
+}
+
+similarity();
+//distance();
+
+void similarity() {
+    Dictionary<int, int> lookup = new Dictionary<int, int>();
+    right.ForEach(x => {
+        lookup.TryAdd(x, 0);
+        lookup[x]++;
+    });
+
+    int similarity = 0;
+    left.ForEach(x => {
+        similarity += (x *lookup.GetValueOrDefault (x, 0));
+    });
+    Console.WriteLine(similarity);
+}
+
+void distance() {
+    left.Sort();
+    right.Sort();
+
+    int allDinstances = 0;
+    for (int i = 0; i < left.Count; i++) {
+        allDinstances += Math.Abs(left[i] - right[i]);
+    }
+    Console.WriteLine(allDinstances);
+}
+
+void parse(string line) {
+    var parts = line.Split("   ");
+    left.Add(int.Parse(parts[0]));
+    right.Add(int.Parse(parts[1]));
+}
