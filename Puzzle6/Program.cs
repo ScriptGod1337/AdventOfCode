50e183daacf671b6cab1712663ed2d65c8e75f9e
Benjamin Klein <hanfdreher@gmail.com>
2025-12-06 11:16:25 +0100

    added solution Puzzle6 day1

---
+++ Puzzle6/Program.cs
@@ -0,0 +1,40 @@
+﻿part1();
+
+#region part1
+void part1() {
+
+    var rows = File.ReadAllLines("input.txt")
+        .Select(line => line
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .ToList())
+        .ToList();
+    var columns = Enumerable.Range(0, rows[0].Count)
+        .Select(col => rows.Select(row => row[col]).ToList())
+        .ToList();
+
+    var sum = columns.Select(calculate).Sum();
+    Console.WriteLine($"sum {sum}");
+
+    long calculate(List<string> colunn) {
+        var colNums = colunn
+            .SkipLast(1)
+            .Select(long.Parse)
+            .ToList();
+
+        long result;
+        switch (colunn.Last()) {
+            case "+":
+                result = colNums.Sum();
+                break;
+            case "*":
+                result = colNums.Aggregate((a, b) => a * b);
+                break;
+            default:
+                throw new Exception("Invalid operation");
+        }
+
+        return result;
+    }
+}
+
+#endregion
